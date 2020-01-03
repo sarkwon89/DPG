2400bc1135fd0e86b6e18334e906ea67e1719cd1
Sarah <sarkwon89@gmail.com>
2020-01-02 22:23:05 -0800

    created html and body

--- index.js
+++ index.js
@@ -1,11 +1,270 @@
-const questions = [
-  
-];
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-function writeToFile(fileName, data) {
- 
+const colors = {
+    green: {
+      wrapperBackground: "#E6E1C3",
+      headerBackground: "#C1C72C",
+      headerColor: "black",
+      photoBorderColor: "#black"
+    },
+    blue: {
+      wrapperBackground: "#5F64D3",
+      headerBackground: "#26175A",
+      headerColor: "white",
+      photoBorderColor: "#73448C"
+    },
+    pink: {
+      wrapperBackground: "#879CDF",
+      headerBackground: "#FF8374",
+      headerColor: "white",
+      photoBorderColor: "#FEE24C"
+    },
+    red: {
+      wrapperBackground: "#DE9967",
+      headerBackground: "#870603",
+      headerColor: "white",
+      photoBorderColor: "white"
+    }
+  };
+
+// variable for profile image
+let profileimage;
+// variable user name
+let userName;
+// variable link to user location
+let userLocation
+// variable link to github profile
+let userGithubProfile
+// variable link to user blog
+let userBlog
+// variable user bio
+let userBio
+// variable # of public repositories
+let numRepo
+// variable # of followers
+let numFollowers
+// variable # of GitHub stars
+let numGithubStars
+// variable # of user followings
+let numUsersfollowing
+// store user's favorite color
+let userFavColor
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([{
+                message: "Enter you GitHub username",
+                name: "username"
+            },
+            {
+                type: "input",
+                name: "color",
+                message: "What is your favorite color?"
+            }
+        ])
+        .then(function ({
+            username,
+            color
+        }) {
+            const queryUrl = `https://api.github.com/users/${username}`;
+            console.log(username);
+            console.log(color);
+            userFavColor = color;
+
+            axios.get(queryUrl).then(function (result) {
+                profileimage = result.data.avatar_url;
+                userName = result.data.login;
+                // userLocation = 
+                userGithubProfile = result.data.userLocation;
+                userBlog = result.data.blog;
+                userBio = result.data.bio;
+                numRepo = result.data.public_repos
+                numFollowers = result.data.followers;
+                // numGithubStars = 
+                numUsersfollowing = result.data.following;
+                console.log(result.data);
+            });
+        });
 }
 
-function init() {
+let secondHalfHtml = `
+<body>
+<h1> name ${userName}`
 
-init();
+function generateHTML(data) {
+    return `<!DOCTYPE html>
+  <html lang="en">
+     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+        <title>Document</title>
+        <style>
+            @page {
+              margin: 0;
+            }
+           *,
+           *::after,
+           *::before {
+           box-sizing: border-box;
+           }
+           html, body {
+           padding: 0;
+           margin: 0;
+           }
+           html, body, .wrapper {
+           height: 100%;
+           }
+           .wrapper {
+           background-color: ${colors[userFavColor].wrapperBackground};
+           padding-top: 100px;
+           }
+           body {
+           background-color: white;
+           -webkit-print-color-adjust: exact !important;
+           font-family: 'Cabin', sans-serif;
+           }
+           main {
+           background-color: #E9EDEE;
+           height: auto;
+           padding-top: 30px;
+           }
+           h1, h2, h3, h4, h5, h6 {
+           font-family: 'BioRhyme', serif;
+           margin: 0;
+           }
+           h1 {
+           font-size: 3em;
+           }
+           h2 {
+           font-size: 2.5em;
+           }
+           h3 {
+           font-size: 2em;
+           }
+           h4 {
+           font-size: 1.5em;
+           }
+           h5 {
+           font-size: 1.3em;
+           }
+           h6 {
+           font-size: 1.2em;
+           }
+           .photo-header {
+           position: relative;
+           margin: 0 auto;
+           margin-bottom: -50px;
+           display: flex;
+           justify-content: center;
+           flex-wrap: wrap;
+           background-color: ${colors[userFavColor].headerBackground};
+           color: ${colors[userFavColor].headerColor};
+           padding: 10px;
+           width: 95%;
+           border-radius: 6px;
+           }
+           .photo-header img {
+           width: 250px;
+           height: 250px;
+           border-radius: 50%;
+           object-fit: cover;
+           margin-top: -75px;
+           border: 6px solid ${colors[userFavColor].photoBorderColor};
+           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+           }
+           .photo-header h1, .photo-header h2 {
+           width: 100%;
+           text-align: center;
+           }
+           .photo-header h1 {
+           margin-top: 10px;
+           }
+           .links-nav {
+           width: 100%;
+           text-align: center;
+           padding: 20px 0;
+           font-size: 1.1em;
+           }
+           .nav-link {
+           display: inline-block;
+           margin: 5px 10px;
+           }
+           .workExp-date {
+           font-style: italic;
+           font-size: .7em;
+           text-align: right;
+           margin-top: 10px;
+           }
+           .container {
+           padding: 50px;
+           padding-left: 100px;
+           padding-right: 100px;
+           }
+  
+           .row {
+             display: flex;
+             flex-wrap: wrap;
+             justify-content: space-between;
+             margin-top: 20px;
+             margin-bottom: 20px;
+           }
+  
+           .card {
+             padding: 20px;
+             border-radius: 6px;
+             background-color: ${colors[userFavColor].headerBackground};
+             color: ${colors[userFavColor].headerColor};
+             margin: 20px;
+           }
+           
+           .col {
+           flex: 1;
+           text-align: center;
+           }
+  
+           a, a:hover {
+           text-decoration: none;
+           color: inherit;
+           font-weight: bold;
+           }
+  
+           @media print { 
+            body { 
+              zoom: .75; 
+            } 
+           }
+        </style>`
+          }
+
+promptUser()
+  .then(function(answers) {
+    const html = generateHTML(answers);
+
+    return writeFileAsync("index.html", html);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+
+// const questions = [
+
+// ];
+
+// function writeToFile(fileName, data) {
+
+// }
+
+// function init() {
+
+//     init();
